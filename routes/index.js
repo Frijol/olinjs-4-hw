21889914d358b6e65d38d986a0a29d7a0db80cce
Frijol <ifoundthemeaningoflife@gmail.com>
2013-12-24 11:16:24 -0800

    yay basically working just need to stop folks from posting namelessly

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
-/*
-* GET home page.
-*/
-
 Twit = require('../models/twit.js');
-var alltwits;
-
-var twits = Twit.find({}, function (err, docs) {
-	if (err) throw err;
-	alltwits = docs
-})
 
 exports.index = function(req, res){
-	res.render('index', {docs: alltwits});
+	var twits = Twit.find({}, function (err, docs) {
+		if (err) throw err;
+		alltwits = docs.reverse()
+		res.render('index', {docs: alltwits});
+	});
 }
